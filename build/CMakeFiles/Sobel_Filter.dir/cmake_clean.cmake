2283d625d3ab36016ec7ccb8bb0c0e8954596ce9
MyWurger <wdgwdg9@gmail.com>
2024-03-10 02:51:36 +0300

    Добавлена реализация фильтра Собеля для нескольких потоков

---
+++ build/CMakeFiles/Sobel_Filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sobel_Filter.dir/main.cpp.o"
+  "Sobel_Filter"
+  "Sobel_Filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sobel_Filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
